4b2ad6b39df6090e16d716b56eb246efc3a4af5b
Andrew Hill <anjo.hill@gmail.com>
2017-03-19 19:31:08 -0600

    Refactoring playerController.cs. Added unit facing when moving.

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
-	// offset is used to correct the player sprite onto the map grid.
+	// offset is used to center the player sprite onto the map grid.
 	private Vector2 offset;
 	// numerical value for length between two tile centers.
 	private float tileWidth, tileHeight;
 	// combined value. Used to apply movement
 	private float deltaX, deltaY;
 	private Vector2 dXVector, dYVector;
+	// current value of how many FixedUpdate() remain before destination is reached.
 	private int frameCount;
+	// total value of how many FixedUpdate() are required to reach destination.
+	// the higher the number, the slower the gnome moves.
+	private int maxFrames;
 
-	//test Vars
 	private Vector2 dest;
 	private Vector2 shortMove;
 	private Rigidbody2D myRigidbody;
@@ -23,35 +26,25 @@ public class playerController : MonoBehaviour {
 		offset = new Vector2 (0.16f, 0.16f);
 		tileWidth = 0.16f;
 		tileHeight = 0.16f;
+
 		deltaX = offset.x + tileWidth;
 		deltaY = offset.y + tileHeight;
-
 		dXVector = new Vector2 (deltaX, 0);
 		dYVector = new Vector2 (0, deltaY);
 
-		//test Vars
 		dest = new Vector2 (0,0);
-		shortMove = new Vector2 (0, 0);;
+		shortMove = new Vector2 (0, 0);
 
 		myRigidbody = GetComponent<Rigidbody2D> ();
 
 		frameCount = 0;
+		maxFrames = 12;
 		
 	}
 	
 	// Update is called once per frame
-	void Update3 () {
-		if (Input.GetKeyDown (KeyCode.D)) {
-			transform.Translate (deltaX, 0f, 0f);
-		} else if (Input.GetKeyDown (KeyCode.A)) {
-			transform.Translate (deltaX * -1, 0f, 0f);
-		}
+	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.W)) {
-			transform.Translate (0f, deltaY, 0f);
-		} else if (Input.GetKeyDown (KeyCode.S)) {
-			transform.Translate (0f, deltaY * -1, 0f);
-		}
 	}
 
 	// Update is called once per frame
@@ -61,45 +54,64 @@ public class playerController : MonoBehaviour {
 			//transform.Translate (shortMove, 0f, 0f);
 			myRigidbody.MovePosition(new Vector2(myRigidbody.position.x + shortMove.x, myRigidbody.position.y + shortMove.y));
 			frameCount--;
-//		}else if (dest.x < 0) {
-//			dest = new Vector2 (dest.x - shortMove, 0);
-//			//transform.Translate (shortMove, 0f, 0f);
-//			myRigidbody.MovePosition(new Vector2(myRigidbody.position.x + shortMove, myRigidbody.position.y));			
 		}else {
 			dest = new Vector2 (0, 0);
 		}
 
 		CheckInput ();
-	
-
 	}
 
+
+
 	void CheckInput() {
-		if (Input.GetKeyDown (KeyCode.D)) {
-//			if (dest.x == 0 && dest.y == 0) {
-//				dest = new Vector2 (deltaX, 0);
-//				shortMove = dest.x / 15;
-//			}
+		if (Input.GetKeyDown (KeyCode.D)) {	
 			DetermineDest(dXVector);
+			SetUnitFacing ();
 		} else if (Input.GetKeyDown (KeyCode.A)) {
-			DetermineDest (dXVector * -1.0f);
+			DetermineDest (dXVector * -1);
+			SetUnitFacing ();
 		}
 
 		if (Input.GetKeyDown (KeyCode.W)) {
 			DetermineDest (dYVector);
+			SetUnitFacing ();
 		} else if (Input.GetKeyDown (KeyCode.S)) {
-			DetermineDest (dYVector * -1.0f);
+			DetermineDest (dYVector * -1);
+			SetUnitFacing ();
 		}
 	}
 
 	void DetermineDest(Vector2 vector) {
-		// Frames to reach destination if unimpeded
-		int frame = 12;
 
 		if (frameCount==0) {
 			dest = new Vector2 (vector.x, vector.y);
-			shortMove = dest / frame;
-			frameCount = frame;
+			shortMove = dest / maxFrames;
+			frameCount = maxFrames;
+		}
+	}
+
+	void SetUnitFacing() {
+		
+//		if(dest.x < 0){
+//			rotationCorrect = 90 - transform.eulerAngles.z;
+//			transform.Rotate (new Vector3(0,0,rotationCorrect));
+//		}else if(dest.x > 0){
+//			rotationCorrect = 270 - transform.eulerAngles.z;
+//			transform.Rotate (new Vector3(0,0,rotationCorrect));
+//		}
+		SetRotate (dest.x, 90, 270);
+		SetRotate (dest.y, 180, 0);
+	}
+
+	void SetRotate(float destinationVector, float negAngle, float posAngle) {
+		float rotationCorrect;
+
+		if(destinationVector < 0){
+			rotationCorrect = negAngle - transform.eulerAngles.z;
+			transform.Rotate (new Vector3(0,0,rotationCorrect));
+		}else if(destinationVector > 0){
+			rotationCorrect = posAngle - transform.eulerAngles.z;
+			transform.Rotate (new Vector3(0,0,rotationCorrect));
 		}
 	}
 }
