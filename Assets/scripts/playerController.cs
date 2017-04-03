dc60a896ebec095b0923c13c0bafece41042bd2c
Andrew Hill <anjo.hill@gmail.com>
2017-04-03 13:02:34 -0600

    Clean up playerController.cs. Removed commented out code, unused variables/methods, and methodized repeated content.

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -4,326 +4,284 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
-	// offset is used to center the player sprite onto the map grid.
-	private Vector2 offset;
-	// numerical value for length between two tile centers.
-	private float tileWidth, tileHeight;
-	// combined value. Used to apply movement
-	private float deltaX, deltaY;
-	// current value of how many FixedUpdate() remain before destination is reached.
-	private int frameCount;
-	// total value of how many FixedUpdate() are required to reach destination.
-	// the higher the number, the slower the gnome moves.
-	private int maxFrames;
 
-	private Vector2 dest;
-	private Rigidbody2D myRigidbody;
+    /*
+     * Movement Properties
+     */
+    // numerical value for length between two tile centers (or, equally, the length of 1 tile).
+    private float tileSize;
+    // The change in movement desired from one keypress
+    private float deltaPos;
+    // the rigidbody of the player object. Used to apply movement.
+    private Rigidbody2D myRigidbody;
 
-	public GameObject topFeeler, botFeeler, rightFeeler, leftFeeler;
-	private feelerController topFeelerScript, botFeelerScript, rightFeelerScript, leftFeelerScript;
+    /*
+     * Collision Properties
+     */
+    // Attached objects representing the next possible movement locations.
+    // If the feelers encounter an obstacle, movement is disabled.
+    public GameObject topFeeler, botFeeler, rightFeeler, leftFeeler;
 
-	public GameObject pVision;
-	private int visionCount;
-	private GameObject farTopVis, farBotVis, farLeftVis, farRightVis;
+    /*
+     * Vision Properties
+     */
+    // Attached Prefab for vision gameobjects
+    public GameObject pVision;
+    // Property holding the current furthest vision object for each cardinal
+    // direction. Used in update loop to extend Line of Sight to max range.
+    private GameObject farTopVis, farBotVis, farLeftVis, farRightVis;
+    // Attache objects of the players immediate vision
+    public GameObject tVis, rVis, bVis, lVis;
+    public GameObject tlVis, trVis, blVis, brVis, cVis;
 
-	public GameObject tVis, rVis, bVis, lVis;
-	public GameObject tlVis, trVis, blVis, brVis, cVis;
+    enum Direction {
+        Up, Down, Left, Right
+    };
 
+    private Direction curFacing;
 
-	private SpriteRenderer sprite;
+    // Use this for initialization
+    void Start() {
+        tileSize = 1;
+        deltaPos = tileSize;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        ResetFarVision();
+    }
 
-	enum Direction {
-		Up, Down, Left, Right
-	};
+    // Update is called once per frame
+    void Update() {
+        CheckInput();
+        GrantAllVision();
+    }
 
-	private Direction curFacing;
-
-	// Use this for initialization
-	void Start () {
-
-		sprite = GetComponent<SpriteRenderer> ();
-		Debug.Log (sprite);
-		Debug.Log ("Player Start Method");
-		//offset = new Vector2 (0.16f, 0.16f);
-		offset = new Vector2 (0, 0);
-		tileWidth = 1;
-		tileHeight = 1;
-
-		deltaX = offset.x + tileWidth;
-		deltaY = offset.y + tileHeight;
-
-		dest = new Vector2 (0,0);
-
-		myRigidbody = GetComponent<Rigidbody2D> ();
-
-		frameCount = 0;
-		maxFrames = 12;
-
-		topFeelerScript = topFeeler.GetComponent<feelerController> ();
-		botFeelerScript = botFeeler.GetComponent<feelerController> ();
-		rightFeelerScript = rightFeeler.GetComponent<feelerController> ();
-		leftFeelerScript = leftFeeler.GetComponent<feelerController> ();
-
-
-		farTopVis = cVis;
-		farBotVis = cVis;
-		farLeftVis = cVis;
-		farRightVis = cVis;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		CheckInput ();	
-		GrantVision (farTopVis, Direction.Up);
-		GrantVision (farBotVis, Direction.Down);
-		GrantVision (farLeftVis, Direction.Left);
-		GrantVision (farRightVis, Direction.Right);
-		//CleanUpVision ();
-
-	}
-
-	// Update is called once per frame
-	void FixedUpdate () {
-//		if (frameCount!=0) {
-//			dest = new Vector2 (dest.x - shortMove.x, dest.y - shortMove.y);
-//			//transform.Translate (shortMove, 0f, 0f);
-//			myRigidbody.MovePosition(new Vector2(myRigidbody.position.x + shortMove.x, myRigidbody.position.y + shortMove.y));
-//			frameCount--;
-//		}else {
-//			dest = new Vector2 (0, 0);
-//		}
-
-	}
-
-	void LateUpdate() {
-
-	}
-
-
-
-	void CheckInput() {
-		Vector2 tempCoord = new Vector2 ();
-		if (Input.GetKeyDown (KeyCode.D)) {	
-//			DetermineDest(dXVector);
-//			SetUnitFacing ();
-			curFacing = Direction.Right;
-			SetRotate ( 90, 270);
-			if (rightFeelerScript.GetCollisionStatus () == false) {
-				tempCoord.Set (myRigidbody.position.x + deltaX, myRigidbody.position.y);
-				ChangePosition (tempCoord);
-			}
-
-		} else if (Input.GetKeyDown (KeyCode.A)) {
-//			DetermineDest (dXVector * -1);
-//			SetUnitFacing ();
-			curFacing = Direction.Left;
-			SetRotate ( 90, 270);
-			if (leftFeelerScript.GetCollisionStatus () == false) {
-				tempCoord.Set (myRigidbody.position.x - deltaX, myRigidbody.position.y);
-				ChangePosition (tempCoord);
-			}
-		}
-
-		if (Input.GetKeyDown (KeyCode.W)) {
-//			DetermineDest (dYVector);
-//			SetUnitFacing ();
-			curFacing = Direction.Up;
-			SetRotate ( 180, 0);
-			if (topFeelerScript.GetCollisionStatus () == false) {
-				tempCoord.Set (myRigidbody.position.x, myRigidbody.position.y + deltaY);
-				ChangePosition (tempCoord);
-			}
-		} else if (Input.GetKeyDown (KeyCode.S)) {
-//			DetermineDest (dYVector * -1);
-//			SetUnitFacing ();
-			curFacing = Direction.Down;
-			SetRotate ( 180, 0);
-			if (botFeelerScript.GetCollisionStatus () == false) {
-				tempCoord.Set (myRigidbody.position.x, myRigidbody.position.y - deltaY);
-				ChangePosition (tempCoord);
-			}
-		}
-
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            //CenterGnome();
+    void CheckInput() {
+        Vector2 tempCoord = new Vector2();
+        if (Input.GetKeyDown(KeyCode.D)) {
+            ExecuteMovement(Direction.Right);
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            ExecuteMovement(Direction.Left);
         }
-	}
 
-	void DetermineDest(Vector2 vector) {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            ExecuteMovement(Direction.Up);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            ExecuteMovement(Direction.Down);
+        }
 
-		if (frameCount==0) {
-			dest = new Vector2 (vector.x, vector.y);
-			frameCount = maxFrames;
-		}
-	}
+    }
 
-	void ChangePosition(Vector2 newCoord) {
-		//oldCoord.Set (myRigidbody.position.x, myRigidbody.position.y);
-		myRigidbody.MovePosition(newCoord);
-		MoveAllFeelers (newCoord);
-		MoveCornerFeelers (newCoord);
+    void ChangePosition(Vector2 newCoord) {
+        myRigidbody.MovePosition(newCoord);
+        MoveAllFeelers(newCoord);
+        MoveCornerFeelers(newCoord);
 
-		CleanUpVision ();
-		//topFeeler.transform.position.Set (newCoord.x, newCoord.y + .32f, topFeeler.transform.position.z);
-		//topFeeler.riposition.Set(newCoord.x, newCoord.y + .32f, topFeeler.transform.position.z);
-	}
+        CleanUpVision();
+    }
 
-	void MoveFeeler(GameObject feeler, Vector2 newCoord) {
-		Rigidbody2D rbFeel = feeler.GetComponent<Rigidbody2D>();
-		rbFeel.MovePosition (newCoord);
-	}
+    void MoveFeeler(GameObject feeler, Vector2 newCoord) {
+        Rigidbody2D rbFeel = feeler.GetComponent<Rigidbody2D>();
+        rbFeel.MovePosition(newCoord);
+    }
 
-	void MoveCornerFeelers(Vector2 newCoord) {
-		Vector2 tVec = new Vector2 (newCoord.x, newCoord.y + tileHeight);
-		Vector2 rVec = new Vector2 (newCoord.x + tileWidth, newCoord.y);
-		Vector2 bVec = new Vector2 (newCoord.x, newCoord.y - tileHeight);
-		Vector2 lVec = new Vector2 (newCoord.x - tileWidth, newCoord.y);
-		Vector2 tlVec = new Vector2 (newCoord.x - tileWidth, newCoord.y + tileHeight);
+    void MoveCornerFeelers(Vector2 newCoord) {
+        Vector2 tVec = new Vector2(newCoord.x, newCoord.y + tileSize);
+        Vector2 rVec = new Vector2(newCoord.x + tileSize, newCoord.y);
+        Vector2 bVec = new Vector2(newCoord.x, newCoord.y - tileSize);
+        Vector2 lVec = new Vector2(newCoord.x - tileSize, newCoord.y);
+        Vector2 tlVec = new Vector2(newCoord.x - tileSize, newCoord.y + tileSize);
 
-		Vector2 trVec = new Vector2 (newCoord.x + tileWidth, newCoord.y + tileHeight);
+        Vector2 trVec = new Vector2(newCoord.x + tileSize, newCoord.y + tileSize);
 
-		Vector2 blVec = new Vector2 (newCoord.x - tileWidth, newCoord.y - tileHeight);
+        Vector2 blVec = new Vector2(newCoord.x - tileSize, newCoord.y - tileSize);
 
-		Vector2 brVec = new Vector2 (newCoord.x + tileWidth, newCoord.y - tileHeight);
+        Vector2 brVec = new Vector2(newCoord.x + tileSize, newCoord.y - tileSize);
 
-		MoveFeeler (tlVis, tlVec);
-		MoveFeeler (trVis, trVec);
-		MoveFeeler (blVis, blVec);
-		MoveFeeler (brVis, brVec);
-		MoveFeeler (tVis, tVec);
-		MoveFeeler (rVis, rVec);
-		MoveFeeler (bVis, bVec);
-		MoveFeeler (lVis, lVec);
-		MoveFeeler (cVis, newCoord);
-	}
+        MoveFeeler(tlVis, tlVec);
+        MoveFeeler(trVis, trVec);
+        MoveFeeler(blVis, blVec);
+        MoveFeeler(brVis, brVec);
+        MoveFeeler(tVis, tVec);
+        MoveFeeler(rVis, rVec);
+        MoveFeeler(bVis, bVec);
+        MoveFeeler(lVis, lVec);
+        MoveFeeler(cVis, newCoord);
+    }
 
-	void MoveAllFeelers(Vector2 newCoord) {
-		Vector2 topVec = new Vector2 (newCoord.x, newCoord.y + tileHeight);
+    void MoveAllFeelers(Vector2 newCoord) {
+        Vector2 topVec = new Vector2(newCoord.x, newCoord.y + tileSize);
 
-		Vector2 botVec = new Vector2 (newCoord.x, newCoord.y - tileHeight);
+        Vector2 botVec = new Vector2(newCoord.x, newCoord.y - tileSize);
 
-		Vector2 rightVec = new Vector2 (newCoord.x + tileWidth, newCoord.y);
+        Vector2 rightVec = new Vector2(newCoord.x + tileSize, newCoord.y);
 
-		Vector2 leftVec = new Vector2 (newCoord.x - tileWidth, newCoord.y);
+        Vector2 leftVec = new Vector2(newCoord.x - tileSize, newCoord.y);
 
-		MoveFeeler (topFeeler, topVec);
-		MoveFeeler (botFeeler, botVec);
-		MoveFeeler (rightFeeler, rightVec);
-		MoveFeeler (leftFeeler, leftVec);
-	}
+        MoveFeeler(topFeeler, topVec);
+        MoveFeeler(botFeeler, botVec);
+        MoveFeeler(rightFeeler, rightVec);
+        MoveFeeler(leftFeeler, leftVec);
+    }
 
-	void SetUnitFacing() {
-		
-//		if(dest.x < 0){
-//			rotationCorrect = 90 - transform.eulerAngles.z;
-//			transform.Rotate (new Vector3(0,0,rotationCorrect));
-//		}else if(dest.x > 0){
-//			rotationCorrect = 270 - transform.eulerAngles.z;
-//			transform.Rotate (new Vector3(0,0,rotationCorrect));
-//		}
-		SetRotate (dest.x, 90, 270);
-		SetRotate (dest.y, 180, 0);
-	}
+    void SetRotate(float negAngle, float posAngle) {
+        float rotationCorrect;
 
-	void SetRotate(float destinationVector, float negAngle, float posAngle) {
-		float rotationCorrect;
+        if (curFacing == Direction.Left || curFacing == Direction.Down) {
+            rotationCorrect = negAngle - transform.eulerAngles.z;
+            transform.Rotate(new Vector3(0, 0, rotationCorrect));
+        } else if (curFacing == Direction.Right || curFacing == Direction.Up) {
+            rotationCorrect = posAngle - transform.eulerAngles.z;
+            transform.Rotate(new Vector3(0, 0, rotationCorrect));
+        }
+    }
 
-		if(destinationVector < 0){
-			rotationCorrect = negAngle - transform.eulerAngles.z;
-			transform.Rotate (new Vector3(0,0,rotationCorrect));
-		}else if(destinationVector > 0){
-			rotationCorrect = posAngle - transform.eulerAngles.z;
-			transform.Rotate (new Vector3(0,0,rotationCorrect));
-		}
-	}
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
 
-	void SetRotate(float negAngle, float posAngle) {
-		float rotationCorrect;
+    void GrantVision(GameObject vision, Direction dir) {
+        visionController sVision = vision.GetComponent<visionController>();
+        Vector3 nextPos = new Vector3();
+        switch (dir) {
+            case Direction.Up:
+                nextPos = new Vector3(vision.transform.position.x, vision.transform.position.y + 1, vision.transform.position.z);
+                break;
+            case Direction.Down:
+                nextPos = new Vector3(vision.transform.position.x, vision.transform.position.y - 1, vision.transform.position.z);
+                break;
+            case Direction.Left:
+                nextPos = new Vector3(vision.transform.position.x - 1, vision.transform.position.y, vision.transform.position.z);
+                break;
+            case Direction.Right:
+                nextPos = new Vector3(vision.transform.position.x + 1, vision.transform.position.y, vision.transform.position.z);
+                break;
 
-		if(curFacing == Direction.Left || curFacing == Direction.Down){
-			rotationCorrect = negAngle - transform.eulerAngles.z;
-			transform.Rotate (new Vector3(0,0,rotationCorrect));
-		}else if(curFacing == Direction.Right || curFacing == Direction.Up){
-			rotationCorrect = posAngle - transform.eulerAngles.z;
-			transform.Rotate (new Vector3(0,0,rotationCorrect));
-		}
-	}
+        }
 
-	void OnTriggerEnter2D (Collider2D col) {
-//		Debug.Log ("Player Triggered.");
-//		RevertPosition ();
-	}
+        if (sVision.firstFixedUpdate == true) {
+            if (sVision.GetCollisionStatus() == false) {
+                GameObject cloneVision = Instantiate(pVision, nextPos, new Quaternion());
+                cloneVision.tag = "ray";
 
-	public Vector3 GetPosition() {
-		return transform.position;
-	}
+                switch (dir) {
+                    case Direction.Up:
+                        farTopVis = cloneVision;
+                        break;
+                    case Direction.Down:
+                        farBotVis = cloneVision;
+                        break;
+                    case Direction.Left:
+                        farLeftVis = cloneVision;
+                        break;
+                    case Direction.Right:
+                        farRightVis = cloneVision;
+                        break;
+                }
+            }
+        }
+    }
 
-	void GrantVision(GameObject vision, Direction dir) {
-		visionController sVision = vision.GetComponent<visionController> ();
-		Vector3 nextPos = new Vector3();
-		switch(dir) {
-		case Direction.Up:
-			nextPos = new Vector3 (vision.transform.position.x, vision.transform.position.y + 1, vision.transform.position.z);
-			break;
-		case Direction.Down:
-			nextPos = new Vector3 (vision.transform.position.x, vision.transform.position.y - 1, vision.transform.position.z);
-			break;
-		case Direction.Left:
-			nextPos = new Vector3 (vision.transform.position.x - 1, vision.transform.position.y, vision.transform.position.z);
-			break;
-		case Direction.Right:
-			nextPos = new Vector3 (vision.transform.position.x + 1, vision.transform.position.y, vision.transform.position.z);
-			break;
-			
-		}
+    void CleanUpVision() {
+        GameObject[] visionClones = new GameObject[0];
 
-		if (sVision.firstFixedUpdate == true) {
-			if (sVision.GetCollisionStatus () == false) {
-				GameObject cloneVision = Instantiate (pVision, nextPos, new Quaternion ());
-				cloneVision.tag = "ray";
-				visionCount++;
+        visionClones = GameObject.FindGameObjectsWithTag("ray");
 
-				switch (dir) {
-				case Direction.Up:
-					farTopVis = cloneVision;
-					break;
-				case Direction.Down:
-					farBotVis = cloneVision;
-					break;
-				case Direction.Left:
-					farLeftVis = cloneVision;
-					break;
-				case Direction.Right:
-					farRightVis = cloneVision;
-					break;
-				}
-			}
-		}
-	}
+        foreach (GameObject visionClone in visionClones) {
+            visionController sClone = visionClone.GetComponent<visionController>();
+            Rigidbody2D rbClone = visionClone.GetComponent<Rigidbody2D>();
+            sClone.toDestroy = true;
+            rbClone.MovePosition(new Vector2(-100, -100));
+        }
 
-	void CleanUpVision() {
-		GameObject[] visionClones = new GameObject[0];
+        visionController sVision = cVis.GetComponent<visionController>();
+        sVision.firstFixedUpdate = false;
 
-		visionClones = GameObject.FindGameObjectsWithTag("ray");
+        ResetFarVision();
 
-		foreach(GameObject visionClone in visionClones) {
-			visionController sClone = visionClone.GetComponent<visionController> ();
-			Rigidbody2D rbClone = visionClone.GetComponent<Rigidbody2D> ();
-//			Collider2D cClone = visionClone.GetComponent<Collider2D> ();
-			sClone.toDestroy = true;
-			rbClone.MovePosition(new Vector2(-100, -100));
-//			cClone.enabled = false;
-		}
+    }
 
-		visionController sVision = cVis.GetComponent<visionController> ();
-		sVision.firstFixedUpdate = false;
+    void ResetFarVision() {
+        farTopVis = cVis;
+        farBotVis = cVis;
+        farLeftVis = cVis;
+        farRightVis = cVis;
+    }
 
+    void GrantAllVision() {
+        GrantVision(farTopVis, Direction.Up);
+        GrantVision(farBotVis, Direction.Down);
+        GrantVision(farLeftVis, Direction.Left);
+        GrantVision(farRightVis, Direction.Right);
+    }
 
-		farTopVis = cVis;
-		farBotVis = cVis;
-		farLeftVis = cVis;
-		farRightVis = cVis;
-	}
+    void ExecuteMovement(Direction dir)
+    {
+        Vector2 tempCoord = new Vector2();
+        curFacing = dir;
+        GameObject curFeeler = DetermineFeeler(dir);
+        Vector2 offsetAngle = DetermineOffsetAngles(dir);
+        Vector2 curDelta = DetermineDelta(dir);
+        SetRotate(offsetAngle.x, offsetAngle.y);
+        if (curFeeler.GetComponent<feelerController>().GetCollisionStatus() == false)
+        {
+            tempCoord.Set(myRigidbody.position.x + curDelta.x, myRigidbody.position.y + curDelta.y);
+            ChangePosition(tempCoord);
+        }
+    }
+
+    Vector2 DetermineOffsetAngles(Direction dir)
+    {
+        Vector2 curVector = new Vector2();
+        if (dir == Direction.Left || dir == Direction.Right)
+        {
+            curVector = new Vector2(90, 270);
+        } else if(dir == Direction.Up || dir == Direction.Down)
+        {
+            curVector = new Vector2(180, 0);
+        }
+
+        return curVector;
+    }
+
+    GameObject DetermineFeeler(Direction dir)
+    {
+        GameObject curFeeler = new GameObject();
+
+        switch(dir)
+        {
+            case Direction.Up:
+                curFeeler = topFeeler;
+                break;
+            case Direction.Down:
+                curFeeler = botFeeler;
+                break;
+            case Direction.Left:
+                curFeeler = leftFeeler;
+                break;
+            case Direction.Right:
+                curFeeler = rightFeeler;
+                break;
+        }
+        return curFeeler;
+    }
+
+    Vector2 DetermineDelta(Direction dir)
+    {
+        Vector2 returnVec = new Vector2();
+        switch(dir)
+        {
+            case Direction.Up:
+                returnVec.Set(0, deltaPos);
+                break;
+            case Direction.Down:
+                returnVec.Set(0, deltaPos * -1);
+                break;
+            case Direction.Left:
+                returnVec.Set(deltaPos * -1, 0);
+                break;
+            case Direction.Right:
+                returnVec.Set(deltaPos, 0);
+                break;
+        }
+        return returnVec;
+    }
 }
